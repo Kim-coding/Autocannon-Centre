34dd64734c0262e56730b09a781ba16117439aba
김보현 <qhgus5459@naver.com>
2024-05-14 18:07:51 +0900

    Select Stage Window 수정

--- Assets/Script/Window/SelectWindow.cs
+++ Assets/Script/Window/SelectWindow.cs
@@ -6,8 +6,32 @@ using UnityEngine.UI;
 
 public class SelectWindow : MonoBehaviour
 {
+    private WindowManager windowManager;
+
+    private void Start()
+    {
+        windowManager = GetComponentInParent<WindowManager>();
+    }
     public void OnClickStage1()
     {
         SceneManager.LoadScene("1Level");
+    }
+
+    public void OnClickStage2()
+    {
+
+    }
+    public void OnClickStage3()
+    {
+
+    }
+    public void OnClickStage4()
+    {
+
+    }
+
+    public void Backspace()
+    {
+        windowManager.Open(Windows.Start);
     }
 }
