053a5cc8325cc8f435eb46d3c2bc76a2285e9473
김보현 <qhgus5459@naver.com>
2024-05-27 16:20:12 +0900

    TutorialManager

--- Assets/Script/Tutorial/TutorialManager.cs
+++ Assets/Script/Tutorial/TutorialManager.cs
@@ -1,20 +1,37 @@
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class TutorialManager : MonoBehaviour
 {
     public GameObject plane;
-
-    public GameObject tutorial1;
-    public GameObject tutorial2;
-    public GameObject tutorial3;
-    public GameObject tutorial4;
-
+    public TextMeshProUGUI[] tutorial;
+    private int currentTutorialIndex = 0;
 
     private void Awake()
     {
         Time.timeScale = 0f;
+        plane.gameObject.SetActive(true);
+        ShowTutorial(currentTutorialIndex);
+    }
+
+    public void OnClickNext()
+    {
+        currentTutorialIndex++;
+        if (currentTutorialIndex < tutorial.Length)
+        {
+            ShowTutorial(currentTutorialIndex);
+        }
+        else
+        {
+            Debug.Log("모든 튜토리얼이 완료되었습니다!");
+        }
+    }
+
+    private void ShowTutorial(int index)
+    {
+        for (int i = 0; i < tutorial.Length; i++)
+        {
+            tutorial[i].gameObject.SetActive(i == index);
+        }
     }
 }
